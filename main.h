55f18cff0cf95bd03ed37f9e93ec0f19316aa285
Charles Lughas <lordlughas@gmail.com>
2022-10-18 09:07:12 -0700

    Update main.h

--- main.h
+++ main.h
@@ -105,7 +105,6 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
-
 /****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
