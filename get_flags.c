ce3078a8aeb977da52ab976304db272bbe382c62
Ogochidy <109528826+Ogochidy@users.noreply.github.com>
2022-10-18 09:02:27 -0700

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * get_flags - This function calculates active flags
  * @format: Formatted string in which to print the arguments
@@ -27,8 +26,7 @@ int get_flags(const char *format, int *i)
 		if (FLAGS_CH[j] == 0)
 			break;
 	}
-
 	*i = curr_i - 1;
-
+	
 	return (flags);
 }
